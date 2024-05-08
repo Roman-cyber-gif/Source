d3f59a703d92d9941e8a833d831a1a7c18b97609
Roman <korolroman198@gmail.com>
2024-05-08 19:13:28 +0300

    added forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,16 @@
 import "./slider";
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 
 
 window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click',  '.decoration_content > div > div', 'after_click');
+    forms();
 });
 
-function largestOfFour(arr) {
-    // let newArr = [];
-    // for (let i = 0; i < arr.length; i++) {
-    //     let num = 0;
-    //     for (let j = 0; j < arr[i].length; j++) {
-    //         if (arr[i][j] > num || arr[i][j] < 0) {
-    //             num = arr[i][j];
-    //         }
-    //     }
-    //     newArr.push(num);
-    // }
-     return arr.map(Function.apply.bind(Math.max, null));
-  }
-  console.log(largestOfFour(
-    [[4, 5, 1, 3], 
-    [13, 27, 18, 26], 
-    [32, 35, 37, 39], 
-    [1000, 1001, 857, 1]]));
+
 
